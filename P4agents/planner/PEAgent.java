ec0174f385e84677cdffff40d51eb43ec43b46d1
Prasannakumarreddyperam <144049270+Prasannakumarreddyperam@users.noreply.github.com>
2023-11-03 07:43:16 +0530

    Update PEAgent.java -pxp488
    middle step code  method update

--- P4agents/planner/PEAgent.java
+++ P4agents/planner/PEAgent.java
@@ -1,13 +1,5 @@
 package edu.cwru.sepia.agent.planner;
 
-import edu.cwru.sepia.action.Action;
-import edu.cwru.sepia.agent.Agent;
-import edu.cwru.sepia.agent.planner.actions.*;
-import edu.cwru.sepia.environment.model.history.History;
-import edu.cwru.sepia.environment.model.state.ResourceType;
-import edu.cwru.sepia.environment.model.state.State;
-import edu.cwru.sepia.environment.model.state.Template;
-import edu.cwru.sepia.environment.model.state.Unit;
 
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -15,139 +7,146 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.action.ActionFeedback;
+import edu.cwru.sepia.action.ActionResult;
+import edu.cwru.sepia.agent.Agent;
+import edu.cwru.sepia.agent.planner.actions.StripsAction;
+import edu.cwru.sepia.environment.model.history.History;
+import edu.cwru.sepia.environment.model.state.State;
+import edu.cwru.sepia.environment.model.state.Template;
+import edu.cwru.sepia.environment.model.state.Unit;
+
 /**
- * This is an outline of the PEAgent. Implement the provided methods. You may add your own methods and members.
+ * This is an outline of the PEAgent. Implement the provided methods. You may
+ * add your own methods and members.
  */
 public class PEAgent extends Agent {
 
-    // The plan being executed
-    private Stack<StripsAction> plan = null;
+	// The plan being executed
+	private Stack<StripsAction> plan = null;
 
-    // maps the real unit Ids to the plan's unit ids
-    // when you're planning you won't know the true unit IDs that sepia assigns. So you'll use placeholders (1, 2, 3).
-    // this maps those placeholders to the actual unit IDs.
-    private Map<Integer, Integer> peasantIdMap;
-    private int townhallId;
-    private int peasantTemplateId;
+	// maps the real unit Ids to the plan's unit ids
+	// when you're planning you won't know the true unit IDs that sepia assigns. So
+	// you'll use placeholders (1, 2, 3).
+	// this maps those placeholders to the actual unit IDs.
+	private Map<Integer, Integer> peasantIdMap;
+	private int townhallId;
+	private int peasantTemplateId;
 
-    public PEAgent(int playernum, Stack<StripsAction> plan) {
-        super(playernum);
-        peasantIdMap = new HashMap<Integer, Integer>();
-        this.plan = plan;
+	public PEAgent(int playernum, Stack<StripsAction> plan) {
+		super(playernum);
+		peasantIdMap = new HashMap<Integer, Integer>();
+		this.plan = plan;
 
-    }
+	}
+	
+	public Stack<StripsAction> getPlan() {
+		return this.plan;
+	}
 
-    @Override
-    public Map<Integer, Action> initialStep(State.StateView stateView, History.HistoryView historyView) {
-        // gets the townhall ID and the peasant ID
-        for(int unitId : stateView.getUnitIds(playernum)) {
-            Unit.UnitView unit = stateView.getUnit(unitId);
-            String unitType = unit.getTemplateView().getName().toLowerCase();
-            if(unitType.equals("townhall")) {
-                townhallId = unitId;
-            } else if(unitType.equals("peasant")) {
-                peasantIdMap.put(unitId, unitId);
-            }
-        }
+	@Override
+	public Map<Integer, Action> initialStep(State.StateView stateView, History.HistoryView historyView) {
+		// gets the townhall ID and the peasant ID
+		for (int unitId : stateView.getUnitIds(playernum)) {
+			Unit.UnitView unit = stateView.getUnit(unitId);
+			String unitType = unit.getTemplateView().getName().toLowerCase();
+			if (unitType.equals("townhall")) {
+				townhallId = unitId;
+			} else if (unitType.equals("peasant")) {
+				peasantIdMap.put(unitId, unitId);
+			}
+		}
 
-        // Gets the peasant template ID. This is used when building a new peasant with the townhall
-        for(Template.TemplateView templateView : stateView.getTemplates(playernum)) {
-            if(templateView.getName().toLowerCase().equals("peasant")) {
-                peasantTemplateId = templateView.getID();
-                break;
-            }
-        }
+		// Gets the peasant template ID. This is used when building a new peasant with the townhall
+		for (Template.TemplateView templateView : stateView.getTemplates(playernum)) {
+			if (templateView.getName().toLowerCase().equals("peasant")) {
+				peasantTemplateId = templateView.getID();
+				break;
+			}
+		}
 
-        return middleStep(stateView, historyView);
-    }
+		return middleStep(stateView, historyView);
+	}
 
-    /**
-     * This is where you will read the provided plan and execute it. If your plan is correct then when the plan is empty
-     * the scenario should end with a victory. If the scenario keeps running after you run out of actions to execute
-     * then either your plan is incorrect or your execution of the plan has a bug.
-     *
-     * For the compound actions you will need to check their progress and wait until they are complete before issuing
-     * another action for that unit. If you issue an action before the compound action is complete then the peasant
-     * will stop what it was doing and begin executing the new action.
-     *
-	 * To check a unit's progress on the action they were executing last turn, you can use the following:
-     * historyView.getCommandFeedback(playernum, stateView.getTurnNumber() - 1).get(unitID).getFeedback()
-     * This returns an enum ActionFeedback. When the action is done, it will return ActionFeedback.COMPLETED
-     *
-     * Alternatively, you can see the feedback for each action being executed during the last turn. Here is a short example.
-     * if (stateView.getTurnNumber() != 0) {
-     *   Map<Integer, ActionResult> actionResults = historyView.getCommandFeedback(playernum, stateView.getTurnNumber() - 1);
-     *   for (ActionResult result : actionResults.values()) {
-     *     <stuff>
-     *   }
-     * }
-     * Also remember to check your plan's preconditions before executing!
-     */
-    @Override
-    public Map<Integer, Action> middleStep(State.StateView stateView, History.HistoryView historyView) {
-        Map<Integer, Action> actions = new HashMap<>();
+	/**
+	 * This is where you will read the provided plan and execute it. If your plan is
+	 * correct then when the plan is empty the scenario should end with a victory.
+	 * If the scenario keeps running after you run out of actions to execute then
+	 * either your plan is incorrect or your execution of the plan has a bug.
+	 *
+	 * You can create a SEPIA deposit action with the following method
+	 * Action.createPrimitiveDeposit(int peasantId, Direction townhallDirection)
+	 *
+	 * You can create a SEPIA harvest action with the following method
+	 * Action.createPrimitiveGather(int peasantId, Direction resourceDirection)
+	 *
+	 * You can create a SEPIA build action with the following method
+	 * Action.createPrimitiveProduction(int townhallId, int peasantTemplateId)
+	 *
+	 * You can create a SEPIA move action with the following method
+	 * Action.createCompoundMove(int peasantId, int x, int y)
+	 *
+	 * these actions are stored in a mapping between the peasant unit ID executing
+	 * the action and the action you created.
+	 *
+	 * For the compound actions you will need to check their progress and wait until
+	 * they are complete before issuing another action for that unit. If you issue
+	 * an action before the compound action is complete then the peasant will stop
+	 * what it was doing and begin executing the new action.
+	 *
+	 * To check an action's progress you can use the historyview object. Here is a
+	 * short example. if (stateView.getTurnNumber() != 0) { Map<Integer,
+	 * ActionResult> actionResults = historyView.getCommandFeedback(playernum,
+	 * stateView.getTurnNumber() - 1); for (ActionResult result :
+	 * actionResults.values()) { <stuff> } } Also remember to check your plan's
+	 * preconditions before executing!
+	 */
+	@Override
+	public Map<Integer, Action> middleStep(State.StateView stateView, History.HistoryView historyView) {
+		Map<Integer, Action> actionList = new HashMap<>();
+		
+		Map<Integer, ActionResult> actionResults = historyView.getCommandFeedback(playernum, stateView.getTurnNumber() - 1);
+		for(ActionResult actionresult : actionResults.values()) {
+			if(actionresult.getFeedback() == ActionFeedback.FAILED) {
+				actionList.put(actionresult.getAction().getUnitId(), actionresult.getAction());
+			}else if(actionresult.getFeedback() == ActionFeedback.INCOMPLETE) {
+				return actionList;
+			}
+		}
+		if(actionList.isEmpty()) {
+			StripsAction sActions = plan.pop();
+			if(sActions.preconditionsMet(sActions.getParent())) {
+				Action action1 = createSepiaAction(sActions);
+				actionList.put(action1.getUnitId(), action1);
+			}
+		}
+		return actionList;
 
-        // If the plan is empty, means we executed all action no need to anything
-        if (plan.isEmpty()) {
-            return actions;
-        }
+	}
 
-        StripsAction nextAction = plan.peek();
+	/**
+	 * Returns a SEPIA version of the specified Strips Action.
+	 * 
+	 * @param action
+	 *            StripsAction
+	 * @return SEPIA representation of same action
+	 */
+	private Action createSepiaAction(StripsAction action) {
+		return action.returnSepaiaActionBack();
+	}
 
-        // To check weather pre-conditions are met or not
-        if (nextAction.preconditionsMet(new GameState(stateView, playernum, 0, 0, false))) {
-            // After pre-conditions met we can execute
-            Action sepiAction = createSepiaAction(nextAction);
-            actions.put(peasantIdMap.get(nextAction.getUnitID()), sepiAction);
+	@Override
+	public void terminalStep(State.StateView stateView, History.HistoryView historyView) {
+	}
 
-           // to remove executed action from list 
-            plan.pop();
-        } else {
-            // If preconditions are not met, wait and do nothing for now
-        }
+	@Override
+	public void savePlayerData(OutputStream outputStream) {
 
-        return actions;
-    }
+	}
 
-    /**
-     * Returns a SEPIA version of the specified Strips Action.
-     *
-     * You can create a SEPIA deposit action with the following method
-     * Action.createPrimitiveDeposit(int peasantId, Direction townhallDirection)
-     *
-     * You can create a SEPIA harvest action with the following method
-     * Action.createPrimitiveGather(int peasantId, Direction resourceDirection)
-     *
-     * You can create a SEPIA build action with the following method
-     * Action.createPrimitiveProduction(int townhallId, int peasantTemplateId)
-     *
-     * You can create a SEPIA move action with the following method
-     * Action.createCompoundMove(int peasantId, int x, int y)
-     * 
-     * Hint:
-     * peasantId could be found in peasantIdMap
-     *
-     * these actions are stored in a mapping between the peasant unit ID executing the action and the action you created.
-     *
-     * @param action StripsAction
-     * @return SEPIA representation of same action
-     */
-    private Action createSepiaAction(StripsAction action) {
-        return null;
-    }
+	@Override
+	public void loadPlayerData(InputStream inputStream) {
 
-    @Override
-    public void terminalStep(State.StateView stateView, History.HistoryView historyView) {
-
-    }
-
-    @Override
-    public void savePlayerData(OutputStream outputStream) {
-
-    }
-
-    @Override
-    public void loadPlayerData(InputStream inputStream) {
-
-    }
+	}
 }
