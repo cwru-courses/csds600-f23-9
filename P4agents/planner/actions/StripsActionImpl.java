c67f9f9775c33737df5022c314bc723299814b6c
Praneeth0007 <143743401+Praneeth0007@users.noreply.github.com>
2023-11-04 22:02:12 +0530

    Update StripsActionImpl.java

--- P4agents/planner/actions/StripsActionImpl.java
+++ P4agents/planner/actions/StripsActionImpl.java
@@ -1,4 +1,4 @@
-package edu.cwru.sepia.agent.planner.actions;
+;;package edu.cwru.sepia.agent.planner.actions;
 
 import edu.cwru.sepia.action.Action;
 import edu.cwru.sepia.agent.planner.GameState;
@@ -64,33 +64,6 @@ public class StripsActionImpl implements StripsAction{
 		} 
 		return false;
 	}
-	
-	public boolean checkForGold() {
-		if ( peasant.getCargoType() == ResourceType.GOLD && peasant.getCargoAmount() > 0) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
-	public boolean checkForWood() {
-		if (peasant.getCargoType() == ResourceType.WOOD && peasant.getCargoAmount() > 0) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
-	private boolean checkPreConditionsMet(int xPos, int yPos, GameState state, int[][] array) {
-		if (xPos >= state.xExtent || xPos < 0 || yPos >= state.yExtent || yPos < 0) {
-			return false;
-		} else {
-			if (array[xPos][yPos] > 0 && peasant.getCargoAmount() == 0) {
-				return true;
-			}
-			return false;
-		}
-	}
 
 	@Override
 	public GameState apply(GameState state) {
@@ -118,8 +91,7 @@ public class StripsActionImpl implements StripsAction{
 			gameState.heuristic();
 			gameState.plan.add(this);
 			return gameState;
-		}
-		else if(null==this.townHallPos && b==true) {
+		} else if(null==this.townHallPos && b==true) {
 			int goldRemaining = gameState.goldMapArray[xPos1][yPos1];
 			ResourceView rView = state.getResource(xPos1, yPos1, state.resourceNodes);
 			ResourceNode rNode;
@@ -132,18 +104,17 @@ public class StripsActionImpl implements StripsAction{
 				gameState.goldMapArray[xPos1][yPos1] -= 100;
 				unit.setCargo(ResourceType.GOLD, 100);
 				goldRemaining -= 100;
-				rNode = new ResourceNode(ResourceNode.Type.GOLD_MINE, xPos1, yPos1, goldRemaining,
-						rView.getID());
+				rNode = new ResourceNode(ResourceNode.Type.GOLD_MINE, xPos1, yPos1, goldRemaining,rView.getID());
 			}
 			gameState.resourceNodes.remove(rView);
 			gameState.resourceNodes.add(new ResourceView(rNode));
 			ActionSEPIA = Action.createPrimitiveGather(uniView1.getID(), getDir(unit.getxPosition(),unit.getyPosition(), xPos1, yPos1));
 			return setGameStateProperties(gameState, uniView1,unit);
-		}else if(null!=this.townHallPos && b==true) {
+		} else if(null!=this.townHallPos && b==true) {
 			gameState.currentGold+= peasant.getCargoAmount();
 			ActionSEPIA = Action.createPrimitiveDeposit(uniView1.getID(), getDir(unit.getxPosition(),unit.getyPosition(), townHallPos.x, townHallPos.y));
 			return setGameStateProperties(gameState, uniView1,unit);
-		}else if(b==false && null==this.townHallPos) {
+		} else if(b==false && null==this.townHallPos) {
 			int i = gameState.woodmapArray[xPos1][yPos1];
 			ResourceView rView = state.getResource(xPos1, yPos1, state.resourceNodes);
 			ResourceNode rNode;
@@ -151,8 +122,7 @@ public class StripsActionImpl implements StripsAction{
 				gameState.woodmapArray[xPos1][yPos1] = 0;
 				unit.setCargo(ResourceType.WOOD, i);
 				rNode = new ResourceNode(ResourceNode.Type.TREE, xPos1, yPos1, 0, rView.getID());
-			} 
-			else {
+			} else {
 				gameState.woodmapArray[xPos1][yPos1] -= 100;
 				unit.setCargo(ResourceType.WOOD, 100);
 				i = i - 100;
@@ -162,9 +132,20 @@ public class StripsActionImpl implements StripsAction{
 			gameState.resourceNodes.add(new ResourceView(rNode));
 			ActionSEPIA = Action.createPrimitiveGather(uniView1.getID(), getDir(unit.getxPosition(),unit.getyPosition(), xPos1, yPos1));
 			return setGameStateProperties(gameState, uniView1,unit);
-		}else if(b==false && null!=this.townHallPos) {
+		} else if(b==false && null!=this.townHallPos) {
 			ActionSEPIA = Action.createPrimitiveDeposit(uniView1.getID(), getDir(unit.getxPosition(),unit.getyPosition(), townHallPos.x, townHallPos.y));
 			return setGameStateProperties(gameState, uniView1,unit);
+		}
+		return new GameState();
+	}
+	
+	private Direction getDir(int pX, int pY, int gX, int gY) {
+		int x = gX - pX;
+		int y = gY - pY;
+		for (Direction d : Direction.values()) {
+			if(y == d.yComponent() && x == d.xComponent()) {
+				return d;
+			}
 		}
 		return null;
 	}
@@ -173,22 +154,37 @@ public class StripsActionImpl implements StripsAction{
 		gameState.playerUnits.remove(uniView);
 		gameState.playerUnits.add(new UnitView(unit));
 		gameState.currentWood+= peasant.getCargoAmount();
-		double cost = 1;
-		gameState.cost+=cost;
+		gameState.cost = gameState.cost + 1;
 		gameState.heuristic();
 		gameState.plan.add(this);
 		return gameState;
 	}
 	
-	private Direction getDir(int pX, int pY, int gX, int gY) {
-		int x = gX - pX;
-		int y = gY - pY;
-		for (Direction d : Direction.values()) {
-			if(x == d.xComponent() && y == d.yComponent()) {
-				return d;
-			}
+	private boolean checkForGold() {
+		if ( peasant.getCargoType() == ResourceType.GOLD && peasant.getCargoAmount() > 0) {
+			return true;
+		}else {
+			return false;
 		}
-		return null;
+	}
+	
+	private boolean checkForWood() {
+		if (peasant.getCargoType() == ResourceType.WOOD && peasant.getCargoAmount() > 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	private boolean checkPreConditionsMet(int xPos, int yPos, GameState state, int[][] array) {
+		if (xPos >= state.xExtent || xPos < 0 || yPos >= state.yExtent || yPos < 0) {
+			return false;
+		} else {
+			if (array[xPos][yPos] > 0 && peasant.getCargoAmount() == 0) {
+				return true;
+			}
+			return false;
+		}
 	}
 
 	@Override
@@ -198,7 +194,7 @@ public class StripsActionImpl implements StripsAction{
 
 	@Override
 	public Action returnSepaiaActionBack() {
-		return ActionSEPIA;
+		return this.ActionSEPIA;
 	}
 
 }
